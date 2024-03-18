9f634569462e84b19e291e745023ff6e7162c9b1
Roman Kornev <romankrnv@yandex.ru>
2024-03-18 22:15:16 +0300

    Add custom Linked List for HistoryManager; Change Tests for HistoryManager;

--- src/com/kanban/service/InMemoryHistoryManager.java
+++ src/com/kanban/service/InMemoryHistoryManager.java
@@ -3,23 +3,100 @@ package com.kanban.service;
 import com.kanban.model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int MAX_HISTORY_RECORDS = 10;
+    private final Map<Integer, Node> history = new HashMap<>();
+    private Node head;
+    private Node tail;
+    private int size = 0;
 
-    private final List<Task> history = new ArrayList<>();
+    private static class Node {
+        Node next;
+        Node prev;
+        Task task;
 
-    @Override
-    public List<Task> getHistory() {
-        return history;
+        public Node(Node prev, Task task, Node next) {
+            this.next = next;
+            this.task = task;
+            this.prev = prev;
+        }
+    }
+
+    private void linkLast(Task task) {
+        Node oldTail = tail;
+        Node newNode = new Node(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(Task task) {
-        if (history.size() >= MAX_HISTORY_RECORDS) {
-            history.remove(0);
+        if (history.containsKey(task.getId())) {
+            remove(task.getId());
+            history.remove(task.getId());
         }
-        history.add(task);
+        linkLast(task);
+        history.put(task.getId(), tail);
+    }
+
+    public void remove(int id) {
+        if (history.containsKey(id)) {
+            if (head == null) {
+                return;
+            }
+
+            if (head == tail) {
+                head = null;
+                tail = null;
+                size--;
+                return;
+            }
+
+            Node node = history.get(id);
+            if (node == head) {
+                head.next.prev = null;
+                head = head.next;
+                size--;
+                return;
+            }
+            if (node == tail) {
+                tail.prev.next = null;
+                tail = tail.prev;
+                size--;
+                return;
+            }
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            node.prev = null;
+            node.next = null;
+            size--;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private List<Task> getTasks() {
+        List<Task> historyList = new ArrayList<>();
+        Node node = head;
+        while (node != null) {
+            historyList.add(node.task);
+            node = node.next;
+        }
+        return historyList;
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return getTasks();
     }
 }
--- test/com/kanban/service/InMemoryHistoryManagerTest.java
+++ test/com/kanban/service/InMemoryHistoryManagerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InMemoryHistoryManagerTest {
     private HistoryManager historyManager;
@@ -15,24 +16,71 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void add_whenOneRecordExist_thenReturn2() {
-        historyManager.getHistory().add(new Task());
+    void add_whenHistoryIsEmpty_thenReturnSize1() {
+        Task task = new Task();
 
-        historyManager.add(new Task());
-        int actualCount = historyManager.getHistory().size();
+        historyManager.add(task);
+        int actualSize = historyManager.getHistory().size();
 
-        assertEquals(2, actualCount);
+        assertEquals(1, actualSize);
     }
 
     @Test
-    void add_whenRecordsMoreThanMax_thenReturn10() {
-        for (int i = 0; i < 10; i++) {
-            historyManager.add(new Task());
-        }
+    void add_whenRecordAlreadyExist_thenReturnSize1() {
+        Task task = new Task();
+        task.setId(1);
+        historyManager.add(task);
 
-        historyManager.add(new Task());
-        int actualCount = historyManager.getHistory().size();
+        historyManager.add(task);
+        int actualSize = historyManager.getHistory().size();
 
-        assertEquals(10, actualCount);
+        assertEquals(1, actualSize);
+    }
+
+    @Test
+    void remove_whenRecordExist_thenHistoryIsEmpty() {
+        Task task = new Task();
+        task.setId(1);
+        historyManager.add(task);
+
+        historyManager.remove(1);
+        boolean isEmpty = historyManager.getHistory().isEmpty();
+
+        assertTrue(isEmpty);
+    }
+
+    @Test
+    void add_whenRecordsIsHead_thenRemoveRecord() {
+        Task task1 = new Task();
+        Task task2 = new Task();
+        Task task3 = new Task();
+        task1.setId(1);
+        task2.setId(2);
+        task3.setId(3);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        historyManager.add(task1);
+        int curHeadId = historyManager.getHistory().get(0).getId();
+
+        assertEquals(2, curHeadId);
+    }
+
+    @Test
+    void getHistory_when3RecordsExist_thenReturnHistorySize3() {
+        Task task1 = new Task();
+        Task task2 = new Task();
+        Task task3 = new Task();
+        task1.setId(1);
+        task2.setId(2);
+        task3.setId(3);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        int actualSize = historyManager.getHistory().size();
+
+        assertEquals(3, actualSize);
     }
 }
